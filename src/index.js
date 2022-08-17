e22c7a3c7426500361ce1e8a42621453254781f6
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-08-16 23:08:47 -0300

    index.js  completed

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();// fot deploy in heroku
+const express = require("express");// bring express to here
+const cors = require("cors");// bring here cors
+const connectToDatabase = require("./database/database");//bring here database
+
+const userRoute = require("./users/user.route");//bring here users
+const authRoute = require("./auth/auth.route");//bring here auth
+const tweetsRoute = require("./tweets/tweet.route");//bring here tweets
+const swaggerRoute = require("/swagger/swagger.route");//bring here swagger
+
+const port = process.env.PORT || 3001;// we will use door number 3001 because door number 3000 is from frontend. 
+const app = express();// app will receve express
+
+connectToDatabase();// it will connect to database at begin 
+app.use(cors());
+app.use(express.json());//our backend will recognize json
+
+app.use("/users",userRoute);
+app.use("/auth",authRoute);
+app.use("/tweets",tweetsRoute);
+app.use("/api-docs",swaggerRoute);
+
+
+app.listen(port, () => {
+console.log(`Serve working in the door: ${port}`);
+})
