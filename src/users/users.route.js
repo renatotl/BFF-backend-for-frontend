8f53e69cc5bf1e512fcc5ae10f2c60eab3f096d4
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-08-18 21:36:59 -0300

    feat: users.route complete

---
+++ src/users/users.route.js
@@ -0,0 +1,8 @@
+const route = require("express").Router();//Router() execute here
+
+const userController = require("./users.controller");// bring our users.controller to here
+
+route.post("/create", userController.createUserController);
+route.get("/",userController.findAllUserController);
+
+module.exports - route;
