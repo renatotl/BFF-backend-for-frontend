e0c86f878eb4893e0fee45084492d7538dacfb03
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-08-20 16:32:34 -0300

    feat: Tweet.js completed

---
+++ src/tweets/Tweet.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+
+const TweetSchema = new mongoose.Schema({
+
+    user: {// who is logged in // relating table in mongoose
+        type: mongoose.Schema.Types.ObjectId,// user id
+    ref: "User",// references the user table
+    required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Array,
+        required: true,
+    },
+    comments: {
+        type: String,
+        required: true,
+    },
+    retweets: {
+        type: Array,
+        required: true,
+    },
+
+})
+
+
+const Tweet = mongoose.model("Tweet",TweetSchema);
+
+module.exports = Tweet;
