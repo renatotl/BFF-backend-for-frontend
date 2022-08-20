29ca73e617ddece8bb4c232095dd725b1ef7d247
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-08-20 12:48:18 -0300

    feat: auth.controller.js completed

---
+++ src/auth/auth.route.js
@@ -0,0 +1,7 @@
+const route = require("express").Router();
+
+const authController = require("./auth.controller");
+
+route.post("/login", authController.loginController);
+
+module.exports = route;
